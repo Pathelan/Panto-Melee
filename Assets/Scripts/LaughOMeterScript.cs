adc1f6170b98f0c2ac3507aaf60c2a68cec63f50
ShrimpyRose <66629216+ShrimpyRose@users.noreply.github.com>
2024-01-26 05:08:58 +0000

    Laugh-o-meter finished
    The laugh-o-meter now functions as intended, ready for a score system to be added.

--- Assets/Scripts/LaughOMeterScript.cs
+++ Assets/Scripts/LaughOMeterScript.cs
@@ -6,16 +6,26 @@ using UnityEngine.UI;
 public class LaughOMeterScript : MonoBehaviour
 {
     public Slider laughOMeter;
+    public float playerScore=500;
+    public float enemyScore=500;
+    public float maxSize;
+    public float currentFillPercentage;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxSize = playerScore + enemyScore;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        maxSize = playerScore + enemyScore;
+        currentFillPercentage = (playerScore / maxSize)*100;
+
+        if(laughOMeter.value!=currentFillPercentage)
+        {
+            laughOMeter.value = currentFillPercentage;
+        }
     }
 }
