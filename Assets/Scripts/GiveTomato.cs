cbfee934c2ac94b1e0ce51c305d5db097d18bdbe
Pathelan <ashelongstaff@gmail.com>
2024-01-26 05:08:35 +0000

    More Sounds
    - Added Sounds when picking up Tomato

--- Assets/Scripts/GiveTomato.cs
+++ Assets/Scripts/GiveTomato.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class GiveTomato : MonoBehaviour
 {
+    [SerializeField] private GameObject sound;
+
+    [SerializeField] private AudioClip audienceBoo;
+    [SerializeField] private AudioClip audienceGasp;
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            // Sounds
+            GameObject sfx = Instantiate(sound, transform.position, transform.rotation);
+            sfx.GetComponent<AudioSource>().clip = audienceGasp;
+
             // Increase Player's Tomato Count
             other.GetComponent<PlayerThrowTomato>().tomatoesHeld += 1;
+            GameObject.Destroy(gameObject);
+        }
+        
+        if (other.tag == "Enemy")
+        {
+            // Sounds
+            GameObject sfx = Instantiate(sound, transform.position, transform.rotation);
+            sfx.GetComponent<AudioSource>().clip = audienceBoo;
+            print("Should play sound");
+
+            // Increase Enemy's Tomato Count
+            //other.GetComponent<PlayerThrowTomato>().tomatoesHeld += 1;
             GameObject.Destroy(gameObject);
         }
     }
